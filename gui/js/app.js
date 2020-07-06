a82d64eabc017f10ff8766f20b051ec8c332b9d6
Yichen Xu <yichen.x@outlook.com>
2020-07-07 01:04:15 +0800

    Adapt web client functions

--- gui/js/app.js
+++ gui/js/app.js
@@ -1,75 +1,8 @@
-const cities = [
-    {
-        center: [116.40, 39.90],
-        level: 2,
-        name: '北京'
-    },
-    {
-        center: [121.26, 31.12],
-        level: 1,
-        name: '上海'
-    },
-    {
-        center: [106.33, 29.33],
-        level: 0,
-        name: '重庆'
-    },
-    {
-        center: [112.55, 28.12],
-        level: 0,
-        name: '长沙'
-    },
-    {
-        center: [113.23333, 23.16667],
-        level: 1,
-        name: '广州'
-    },
-    {
-        center: [118.78333, 32.05000],
-        level: 0,
-        name: '南京'
-    },
-    {
-        center: [114.31667, 30.51667],
-        level: 2,
-        name: '武汉'
-    },
-    {
-        center: [106.26667, 38.46667],
-        level: 0,
-        name: '银川'
-    },
-    {
-        center: [108.95000, 34.26667],
-        level: 1,
-        name: '西安'
-    },
-    {
-        center: [120.33333, 36.06667],
-        level: 0,
-        name: '青岛'
-    },
-    {
-        center: [102.73333, 25.05000],
-        level: 1,
-        name: '昆明'
-    },
-    {
-        center: [120.15358, 30.287458],
-        level: 2,
-        name: '杭州'
-    },
-    {
-        center: [108.320007, 22.82402],
-        level: 0,
-        name: '南宁'
-    },
-    {
-        center: [91.11450, 29.644150],
-        level: 1,
-        name: "拉萨"
-    }
-]
+Number.prototype.pad = function (size) {
+    var s = String(this);
+    while (s.length < (size || 2)) { s = "0" + s; }
+    return s;
+}
 
 const journeys = [
     {
@@ -130,17 +63,36 @@ const journeys = [
     },
 ]
 
+let mkTickTimer = () => setInterval(() => {
+    vm.now.time = vm.now.time + 1;
+    if (vm.now.time >= 24) {
+        vm.now.time = vm.now.time - 24;
+        vm.now.day = vm.now.day + 1;
+    }
+}, 1000);
+
 var data = {
     cities: [],
-    journeys: journeys,
+    lines: [],
+    journeys: [],
     selectedJourney: 0,
+    now: {
+        day: 0,
+        time: 0
+    },
+    tickTimer: mkTickTimer(),
 
     running: true,
     addingJourney: false,
-    
+
+    nextTid: 1,
+
     newJourney: {
         src: '',
-        dest: ''
+        dest: '',
+        dep_time: '',
+        strategy: '',
+        time_limit: 0,
     },
 
     typeNames: {
@@ -148,6 +100,22 @@ var data = {
         subway: '火车',
         highway: '客车'
     },
+
+    classOfStepStatus: {
+        pending: 'journey-step',
+        finished: 'journey-step journey-step-finished',
+        active: 'journey-step journey-step-active'
+    },
+
+    cachedJourney: {
+        journey: null,
+        form: null,
+        error: false,
+    },
+
+    requestingCache: false,
+
+    socket: null,
 }
 
 // initialzie Vue
@@ -162,12 +130,55 @@ var vm = new Vue({
         handleAddCancelButtonClick: function (e) {
             this.running = true;
             this.addingJourney = false;
-            this.newJourney.src = '';
-            this.newJourney.dest = '';
-        }
+        },
+        handleConfirmButtonClick: function (e) {
+            if (this.cachedJourney.error) {
+                M.toast({ html: '没有可行的行程' });
+                return;
+            }
+
+            const ret = validateForm(this.newJourney);
+            if (!ret[0]) {
+                M.toast({ html: ret[1] });
+            } else {
+                this.socket.send(JSON.stringify({
+                    type: 'add journey',
+                    data: {
+                        jid: this.cachedJourney.journey.jid
+                    }
+                }));
+                this.nextTid = this.nextTid + 1;
+                this.running = true;
+                this.addingJourney = false;
+            }
+        },
+
+        // cache-related
+        requestJourney: function () {
+            const payload = {
+                tid: this.nextTid.pad(3),
+                src: this.newJourney.src,
+                dest: this.newJourney.dest,
+                day: this.now.day,
+                dep_time: this.newJourney.dep_time,
+                time_limit: this.newJourney.time_limit,
+                strategy: this.newJourney.strategy
+            };
+
+            if (payload.dep_time < this.now.time) {
+                payload.day = payload.day + 1;
+            }
+
+            this.socket.send(JSON.stringify({
+                type: 'plan',
+                data: payload,
+            }));
+
+            this.requestingCache = true;
+        },
     },
     computed: {
-        addJourneyCardClass: function() {
+        addJourneyCardClass: function () {
             if (this.addingJourney) {
                 return "card add-journey-card scale-transition scale-out scale-in";
             } else {
@@ -180,6 +191,142 @@ var vm = new Vue({
             } else {
                 return "btn-floating btn-large blue waves-effect waves-light scale-transition scale-out scale-in";
             }
+        },
+        processedCachedJourney: function () {
+            if (this.cacheStatus == 'okay') {
+                return [this.cachedJourney.journey].map(j => {
+                    let ret = Object.assign({}, j);
+                    ret.route = ret.route.map((r, i) => {
+                        let p = Object.assign({}, this.lines[r]);
+                        p.active = false;
+                        p.from = this.cities[p.from];
+                        p.to = this.cities[p.to];
+                        p.risk = ret.step_risk[i];
+                        p.arrive_time = (p.dep_time + p.duration) % 24;
+
+                        return p;
+                    });
+                    ret.src = this.cities[ret.src];
+                    ret.dest = this.cities[ret.dest];
+                    ret.arrive_time = (ret.dep_time + ret.length) % 24;
+                    ret.arrive_day = ret.day + Math.floor((ret.dep_time + ret.length) / 24);
+
+                    ret.status = getJourneyStatus(
+                        ret.dep_time, ret.route, getPassedTime(this.now.day, this.now.time, ret.day, ret.dep_time));
+
+                    ret.isPending = ret.status.state == 'pending';
+                    ret.isFinished = ret.status.state == 'finished';
+                    ret.isOngoing = !(ret.isPending || ret.isFinished);
+
+                    for (let i = 0; i < ret.route.length; i++) {
+                        if (i < ret.status.stage) {
+                            ret.route[i].status = 'finished';
+                        }
+
+                        if (i == ret.status.stage) {
+                            if (ret.status.state == 'waiting' || ret.status.state == 'pending')
+                                ret.route[i].status = 'pending';
+                            else
+                                ret.route[i].status = 'active';
+                        }
+
+                        if (i > ret.status.stage) {
+                            ret.route[i].status = 'pending';
+                        }
+                    }
+
+                    return ret;
+                })[0];
+            }
+        },
+        processedJourneys: function () {
+            return this.journeys.map(j => {
+                let ret = Object.assign({}, j);
+                ret.route = ret.route.map((r, i) => {
+                    let p = Object.assign({}, this.lines[r]);
+                    p.active = false;
+                    p.from = this.cities[p.from];
+                    p.to = this.cities[p.to];
+                    p.risk = ret.step_risk[i];
+                    p.arrive_time = (p.dep_time + p.duration) % 24;
+
+                    return p;
+                });
+                ret.src = this.cities[ret.src];
+                ret.dest = this.cities[ret.dest];
+                ret.arrive_time = (ret.dep_time + ret.length) % 24;
+                ret.arrive_day = ret.day + Math.floor((ret.dep_time + ret.length) / 24);
+
+                ret.status = getJourneyStatus(
+                    ret.dep_time, ret.route, getPassedTime(this.now.day, this.now.time, ret.day, ret.dep_time));
+
+                ret.isPending = ret.status.state == 'pending';
+                ret.isFinished = ret.status.state == 'finished';
+                ret.isOngoing = !(ret.isPending || ret.isFinished);
+
+                for (let i = 0; i < ret.route.length; i++) {
+                    if (i < ret.status.stage) {
+                        ret.route[i].status = 'finished';
+                    }
+
+                    if (i == ret.status.stage) {
+                        if (ret.status.state == 'waiting' || ret.status.state == 'pending')
+                            ret.route[i].status = 'pending';
+                        else
+                            ret.route[i].status = 'active';
+                    }
+
+                    if (i > ret.status.stage) {
+                        ret.route[i].status = 'pending';
+                    }
+                }
+
+                return ret;
+            });
+        },
+        depTimeOptions: function () {
+            var ret = [];
+            for (let i = 0; i < 24; i++) {
+                let label = null;
+                if (i + this.now.time < 24) {
+                    label = ((i + this.now.time) % 24).pad(2) + ':00';
+                } else {
+                    label = ((i + this.now.time) % 24).pad(2) + ':00 (明天)';
+                }
+                ret.push({
+                    value: (i + this.now.time) % 24,
+                    label: label
+                });
+            }
+
+            return ret;
+        },
+        canCache: function () {
+            return this.addingJourney && validateForm(this.newJourney)[0];
+        },
+        cacheStatus: function () {
+            if (this.requestingCache) {
+                return 'requesting';
+            }
+
+            if (!this.cachedJourney.form) {
+                return 'empty';
+            }
+
+            let eq = this.cachedJourney.form.src == this.newJourney.src
+                && this.cachedJourney.form.dest == this.newJourney.dest
+                && this.cachedJourney.form.dep_time == this.newJourney.dep_time
+                && this.cachedJourney.form.time_limit == this.newJourney.time_limit
+                && this.cachedJourney.form.strategy == this.newJourney.strategy;
+
+            if (!eq) {
+                return 'outdated';
+            } else {
+                return 'okay';
+            }
+        },
+        needCache: function () {
+            return !this.requestingCache && this.canCache && ((this.cacheStatus == 'empty' && !this.cachedJourney.error) || this.cacheStatus == 'outdated');
         }
     }
 })
@@ -187,7 +334,7 @@ var vm = new Vue({
 // initialzie Gaode map
 var map = new AMap.Map('map', {
     mapStyle: 'amap://styles/whitesmoke',
-    zoom: 5.3,
+    zoom: 5,
     center: [106.5, 32],
     viewMode: '3D'
 });
@@ -201,32 +348,76 @@ vm.$watch('cities', function (cities, old) {
 vm.$watch('selectedJourney', function (selected, old) {
     if (!selected) {
         map.remove(map.journeyPaths);
+        map.remove(map.travelerMarker);
         map.journeyPaths = [];
+        map.travelerMarker = [];
     } else {
-        for (let i = 0; i < data.journeys.length; i++) {
-            let journey = data.journeys[i];
-            if (journey.id == selected) {
-                let roads = journey.path;
-                renderJourney(roads, map);
+        for (let i = 0; i < vm.processedJourneys.length; i++) {
+            let journey = vm.processedJourneys[i];
+            if (journey.jid == selected) {
+                renderJourney(journey, map);
             }
         }
     }
-})
+});
+
+vm.$watch('processedJourneys', function (journeys, old) {
+    if (this.selectedJourney) {
+        for (let i = 0; i < journeys.length; i++) {
+            let journey = journeys[i];
+            if (journey.jid == vm.selectedJourney) {
+                renderJourney(journey, map);
+            }
+        }
+    }
+
+});
 
 vm.$watch('addingJourney', function (adding, old) {
     if (adding) {
+        this.newJourney.src = '';
+        this.newJourney.dest = '';
+        this.newJourney.dep_time = '';
+        this.newJourney.strategy = '';
+        this.newJourney.time_limit = 0;
+
+        this.cachedJourney.form = null;
+        this.cachedJourney.journey = null;
+        this.cachedJourney.error = false;
+
+        let elems = document.querySelectorAll('select');
+        M.FormSelect.init(elems, {});
         this.addJourneyModal.open();
     } else {
         this.addJourneyModal.close();
     }
-})
+});
 
-data.cities = cities;
+vm.$watch('running', function (running, old) {
+    if (!running) {
+        clearInterval(this.tickTimer);
+    } else {
+        this.tickTimer = mkTickTimer();
+    }
+});
+
+vm.$watch('cities', function (cities, old) {
+    let elems = document.querySelectorAll('select');
+    M.FormSelect.init(elems, {});
+});
+
+vm.$watch('needCache', function (b, old) {
+    if (b) {
+        console.log('updating cache ...')
+        this.cachedJourney.form = Object.assign({}, this.newJourney);
+        this.requestJourney();
+    }
+});
 
 // initialize Materialize
 document.addEventListener('DOMContentLoaded', function () {
     M.AutoInit();
-    let elem = document.querySelector('#journey-list');
+    let elem = document.querySelectorAll('#journey-list');
     M.Collapsible.init(elem, {
         onOpenStart: e => {
             data.selectedJourney = e.id;
@@ -244,3 +435,37 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     vm.addJourneyModal = addJourneyModal;
 });
+
+let socket = new WebSocket('ws://127.0.0.1:5678');
+
+socket.onopen = e => {
+    console.log('Websocket connected')
+    socket.send(JSON.stringify({ type: 'list city' }));
+    socket.send(JSON.stringify({ type: 'list line' }));
+    socket.send(JSON.stringify({ type: 'list journey' }));
+}
+
+socket.onmessage = e => {
+    let msg = JSON.parse(e.data);
+    console.log(msg);
+    if (msg.type == 'list city reply') {
+        vm.cities = msg.data;
+    }
+    if (msg.type == 'list line reply') {
+        vm.lines = msg.data;
+    }
+    if (msg.type == 'list journey reply') {
+        vm.journeys = msg.data;
+    }
+    if (msg.type == 'plan reply') {
+        vm.cachedJourney.error = false;
+        vm.cachedJourney.journey = msg.data;
+        vm.requestingCache = false;
+    }
+    if (msg.type == 'plan nak') {
+        vm.cachedJourney.error = true;
+        vm.requestingCache = false;
+    }
+}
+
+vm.socket = socket;
--- gui/js/utils.js
+++ gui/js/utils.js
@@ -1,17 +1,8 @@
 
-const generateLine = (road, roadColors, alpha) => {
-    let src = road.src;
-    let dest = road.dest;
-    let srcPos = null;
-    let destPos = null;
-    for (let j = 0; j < cities.length; j++) {
-        if (cities[j].name == src) {
-            srcPos = cities[j].center;
-        }
-        if (cities[j].name == dest) {
-            destPos = cities[j].center;
-        }
-    }
+const generateLine = (step, roadColors, alpha) => {
+    let srcPos = step.from.pos;
+    let destPos = step.to.pos;
+
     let midPos = [(srcPos[0] + destPos[0]) / 2, (srcPos[1] + destPos[1]) / 2]
     let cornerPos = [destPos[0], srcPos[1]]
     let ctlPos = [alpha * cornerPos[0] + (1 - alpha) * midPos[0], alpha * cornerPos[1] + (1 - alpha) * midPos[1]]
@@ -23,7 +14,7 @@ const generateLine = (road, roadColors, alpha) => {
         path: path,
         strokeWeight: 5,
         zIndex: 1,
-        strokeColor: roadColors[road.type],
+        strokeColor: roadColors[step.type],
         lineCap: 'round',
         lineJoin: 'round',
         showDir: true
@@ -49,8 +40,8 @@ const generateLine = (road, roadColors, alpha) => {
 //     line.on('mouseover', e => infoWindow.open(map, midPos));
 //     line.on('mouseout', e => infoWindow.close())
 
-function generatePath(roads, roadColors, alpha) {
-    return roads.map(road => generateLine(road, roadColors, alpha));
+function generatePath(steps, roadColors, alpha) {
+    return steps.map(step => generateLine(step, roadColors, alpha));
 }
 
 function renderCities(cities, map) {
@@ -64,7 +55,7 @@ function renderCities(cities, map) {
     for (let i = 0; i < cities.length; i++) {
         let city = cities[i];
         var circleMarker = new AMap.CircleMarker({
-            center: city.center,
+            center: city.pos,
             radius: 5.5,
             strokeWeight: 1.5,
             strokeColor: markerColors[city.level],
@@ -73,7 +64,7 @@ function renderCities(cities, map) {
             zIndex: 10,
             bubble: true,
             cursor: 'pointer',
-            clickable: true,
+            clickable: false,
         })
         circleMarker.setMap(map)
         map.cityMarkers.push(circleMarker);
@@ -92,9 +83,9 @@ function renderCities(cities, map) {
                 'font-family': 'Source Code Pro',
                 'color': '#457b9d',
                 'margin-bottom': '0.5em',
-                'padding': '0'
+                'padding': '0px'
             },
-            position: city.center
+            position: city.pos
         });
 
         text.setMap(map);
@@ -102,17 +93,153 @@ function renderCities(cities, map) {
     }
 }
 
-function renderJourney(roads, map) {
+function renderJourney(journey, map) {
     const roadColors = {
         air: '#a0c4ff',
         subway: '#457b9d',
         highway: '#1d3557'
     }
-    let paths = generatePath(roads, roadColors, 0.05);
+    let paths = generatePath(journey.route, roadColors, 0.05);
     if (map.journeyPaths === undefined) {
         map.journeyPaths = [];
     }
+    if (map.travelerMarker === undefined) {
+        map.travelerMarker = [];
+    }
+
     map.remove(map.journeyPaths);
     map.journeyPaths = paths;
     map.add(paths);
+
+    let markerPos = [];
+    let status = '';
+    if (journey.status.state == 'pending') {
+        // journey has not started
+        markerPos = journey.src.pos;
+        status = '即将出发';
+    } else if (journey.status.state == 'finished') {
+        // journey has finished
+        markerPos = journey.dest.pos;
+        status = '已抵达';
+    } else if (journey.status.state == 'waiting') {
+        // waiting
+        markerPos = journey.route[journey.status.stage].from.pos;
+        status = '正在等候';
+    } else {
+        // on the way
+        let srcPos = journey.route[journey.status.stage].from.pos;
+        let destPos = journey.route[journey.status.stage].to.pos;
+        markerPos = [(srcPos[0] + destPos[0]) / 2, (srcPos[1] + destPos[1]) / 2];
+        status = '在旅途中';
+    }
+    markerPos = [markerPos[0] + 0.0, markerPos[1]];
+
+    var travelerMarker = new AMap.CircleMarker({
+        center: markerPos,
+        radius: 10,
+        strokeOpacity: 0,
+        fillColor: '#48435C',
+        fillOpacity: 0.7,
+        zIndex: 100,
+        bubble: false,
+        cursor: 'pointer',
+        clickable: true,
+    });
+
+    let content = [];
+    content.push('<div class="info-card card"><div class="card-content">');
+    content.push('<span class="journey-field">旅客编号</span><br/>');
+    content.push(journey.tid);
+    content.push('<br/>');
+    content.push('<span class="journey-field">状态</span><br/>');
+    content.push(status);
+    content = content.join('');
+
+    if (map.travelerInfoWindow === undefined) {
+        const infoWindow = new AMap.InfoWindow({
+            isCustom: true,
+            content: content,
+            offset: new AMap.Pixel(0, -30)
+        });
+
+        map.travelerInfoWindow = infoWindow;
+    } else {
+        map.travelerInfoWindow.setContent(content);
+    }
+
+    travelerMarker.on('mouseover', e => map.travelerInfoWindow.open(map, markerPos));
+    travelerMarker.on('mouseout', e => map.travelerInfoWindow.close())
+
+    map.remove(map.travelerMarker);
+    map.travelerMarker = [travelerMarker];
+    map.add([travelerMarker]);
 }
+
+function getPassedTime(day, now, dep_day, dep_time) {
+    return (day - dep_day) * 24 + (now - dep_time);
+}
+
+function timeBetween(t1, t2) {
+    return (t2 - t1 + 24) % 24;
+}
+
+function getJourneyStatus(dep_time, route, passed) {
+    if (passed < 0) {
+        return {
+            stage: 0,
+            state: 'pending'
+        }
+    }
+
+    var t = 0;
+    var now = dep_time;
+    for (var i = 0; i < route.length; i++) {
+        t += timeBetween(now, route[i].dep_time);
+        if (t > passed) {
+            return {
+                stage: i,
+                state: 'waiting'
+            }
+        }
+
+        t += route[i].duration;
+        if (t > passed) {
+            return {
+                stage: i,
+                state: 1 - (t - passed) / route[i].duration
+            }
+        }
+        now = route[i].arrive_time;
+    }
+    return {
+        stage: route.length,
+        state: 'finished'
+    }
+}
+
+function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
+
+const validateForm = form => {
+    if (form.src === '') {
+        return [false, '未选择出发地'];
+    }
+    if (form.dest === '') {
+        return [false, '未选择目的地'];
+    }
+    if (form.dep_time === '') {
+        return [false, '未选择出发时间'];
+    }
+    if (!form.strategy) {
+        return [false, '未选择旅行策略'];
+    }
+    if (form.src == form.dest) {
+        return [false, '出发地与目的地不能相同'];
+    }
+
+    if (!isNumber(form.time_limit)) {
+        return [false, '无效的整数格式'];
+    }
+
+    return [true, ''];
+}
+
